d99bab99d48bd4e0dfcd483540e03a371a65f44c
Yashvi96011 <yashvibaraiya07@gmail.com>
2026-06-16 18:38:23 +0530

    Update FIFO_memory.v

--- FIFO_memory.v
+++ FIFO_memory.v
@@ -8,10 +8,10 @@ module fifo_mem #(
     input  wire [DATA_WIDTH-1:0]    data_in,
     input  wire                     full,
     input  wire                     empty,
-    input  wire                     w_en,
-    input  wire                     r_en,
-    input  wire [ADDR_WIDTH:0]      b_wptr,
-    input  wire [ADDR_WIDTH:0]      b_rptr,
+    input  wire                     wen,
+    input  wire                     ren,
+    input  wire [ADDR_WIDTH:0]      binary_wptr,
+    input  wire [ADDR_WIDTH:0]      binary_rptr,
     output wire [DATA_WIDTH-1:0]    data_out
 );
 
@@ -20,12 +20,44 @@ module fifo_mem #(
 
     // Write operation
     always @(posedge wclk) begin
-        if ((w_en == 1'b1) && (full == 1'b0)) begin
-            fifo_array[b_wptr[ADDR_WIDTH-1:0]] <= data_in;
+        if ((wen == 1'b1) && (full == 1'b0)) begin
+            fifo_array[binary_wptr[ADDR_WIDTH-1:0]] <= data_in;
         end
     end
 
     // Read operation (combinational)
-    assign data_out = fifo_array[b_rptr[ADDR_WIDTH-1:0]];
+    assign data_out = fifo_array[binary_rptr[ADDR_WIDTH-1:0]];
 
-endmodule
+endmodule 
+
+// module fifo_mem #(
+//     parameter DATA_WIDTH = 8,
+//     parameter FIFO_DEPTH = 8,
+//     parameter ADDR_WIDTH = 3
+// )(
+//     input  wire                     wclk,
+//     input  wire                     rclk,
+//     input  wire [DATA_WIDTH-1:0]    data_in,
+//     input  wire                     full,
+//     input  wire                     empty,
+//     input  wire                     w_en,
+//     input  wire                     r_en,
+//     input  wire [ADDR_WIDTH:0]      b_wptr,
+//     input  wire [ADDR_WIDTH:0]      b_rptr,
+//     output wire [DATA_WIDTH-1:0]    data_out
+// );
+
+//     // Memory array
+//     reg [DATA_WIDTH-1:0] fifo_array [0:FIFO_DEPTH-1];
+
+//     // Write operation
+//     always @(posedge wclk) begin
+//         if ((w_en == 1'b1) && (full == 1'b0)) begin
+//             fifo_array[b_wptr[ADDR_WIDTH-1:0]] <= data_in;
+//         end
+//     end
+
+//     // Read operation (combinational)
+//     assign data_out = fifo_array[b_rptr[ADDR_WIDTH-1:0]];
+
+// endmodule
